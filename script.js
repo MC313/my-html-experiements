1834c1438723455584829120479016b53a1a4fc9
Malcolm Childs <michilds0@gmail.com>
2016-10-28 00:25:07 -0400

    updated js file to activate overlay on click

--- script.js
+++ script.js
@@ -1,45 +1,70 @@
- 
 
 
 
 
- $(document).ready(function(){
+
+$(document).ready(function () {
 
     /*(if (typeof jQuery != "undefined") {
 
     	alert('jQuery is installed!');
 
        } else {
-        
+
         alert('jQuery is not installed!');
 
     }*/
+    var overlay1 = document.getElementById('overlay-1');
+    var overlay2 = document.getElementById('overlay-2');
+    var overlay3 = document.getElementById('overlay-3');
+    var overlay4 = document.getElementById('overlay-4');
 
-    //get width and height of splash screen before resizing
-    var coverWidth = $("#container-1").outerWidth();
-    var coverHeight = $("#container-1").outerHeight();
+    var closeIcon = document.getElementsByClassName('close-icon');
+    var closeOverlay = '';
 
-    //get width and height of splash screen before resizing
-    var splashWidth = $("#splash-container").outerWidth();
-    var splashHeight = $("#splash-container").outerHeight();
+    var a = $(overlay1).css('-webkit-clip-path');
+    var b = $(overlay2).css('-webkit-clip-path');
+    var c = $(overlay3).css('-webkit-clip-path');
+    var d = $(overlay4).css('-webkit-clip-path');
 
-    //mouse over function for first arrow icon
-    $("#line-1").click(function() {
-        $("#text-container").hide();
-        $(this).fadeOut(400)
-        $("#splash-container").animate( { width: coverWidth, height: coverHeight } )
-        $("#splash-container").fadeOut(800)
-        $("#line-2").fadeIn(4500)   
-    });
+    openCloseOverlay = function (closeIconValue, overlayElement, initialClipPathValue) {
+        $(overlayElement).click(function () {
+            $(this).css({
+                'z-index': '10',
+                opacity: 1,
+                '-webkit-clip-path': 'inset(0% 0% 0% 0%)'
+            })
+        });
 
-    $("#line-2").click(function() {
-        $("#text-container").hide();
-        $("#text-container").fadeIn(4500)
-        $("#splash-container").fadeIn()
-        $("#splash-container").animate( { width: '100%', height: '100%' } )
-        $("#line-1").fadeIn(2500)
-    });
+        $(closeIconValue).click(function () {
+            var x = $(overlayElement).css({
+                '-webkit-clip-path': initialClipPathValue
+            });
+            console.log($(overlayElement).css('-webkit-clip-path'));
+        });
+    };
 
- }); 
+    // closeOverlay = function (closeIconValue, overlaySelected, initialClipPathValue) {
+    //     $(closeIconValue).click(function () {
+    //         console.log(initialClipPathValue);
+    //         console.log(overlaySelected)
+    //         var x = $(overlaySelected).css({ '-webkit-clip-path': 'inset(0% 0% 55% 50%)' });
+    //         console.log($(x).css('-webkit-clip-path'));
+    //     });
+    // };
 
- 
+    openCloseOverlay(closeIcon[0], overlay1, a);
+    openCloseOverlay(closeIcon[1], overlay2, b);
+    openCloseOverlay(closeIcon[2], overlay3, c);
+    openCloseOverlay(closeIcon[3], overlay4, d);
+
+    // closeOverlay(closeIcon[0], overlay1, a);
+    // closeOverlay(closeIcon[1], overlay2, b);
+    // closeOverlay(closeIcon[2], overlay3, c);
+    // closeOverlay(closeIcon[3], overlay4, d);
+
+
+});
+
+
+
